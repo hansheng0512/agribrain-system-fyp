45bbed46ce8a22162e48362cb9e8d2649fcaa6bb
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-12 22:20:00 +0800

    :sparkles: Setup File Controller

---
+++ server/controller/Famring.js
@@ -0,0 +1,25 @@
+import Farming from "../model/FarmingModel.js";
+
+export const getFarming = (req,res) =>{
+
+}
+
+
+export const getFarmingById = (req,res) =>{
+
+}
+
+
+export const createFarming = (req,res) =>{
+
+}
+
+
+export const updateFarming = (req,res) =>{
+
+}
+
+
+export const deleteFarming = (req,res) =>{
+
+}
---
+++ server/controller/User.js
@@ -0,0 +1,25 @@
+import User from "../model/UserModel.js";
+
+export const getUser = (req,res) =>{
+
+}
+
+
+export const getUserById = (req,res) =>{
+
+}
+
+
+export const createUser = (req,res) =>{
+
+}
+
+
+export const updateUser = (req,res) =>{
+
+}
+
+
+export const deleteUser = (req,res) =>{
+
+}
