692157232ae042324370c360095491783b0f5dfa
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-13 16:20:36 +0800

    :sparkles: Connect Session Sequelize

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import dotenv from 'dotenv'
-// import db from './config/Database.js'
+import SequelizeStore from "connect-session-sequelize";
+import db from './config/Database.js'
 import UserRoute from "./route/UserRoute.js";
 import FarmingRoute from "./route/FarmingRoute.js";
 import CropRoute from "./route/CropRoute.js";
@@ -11,6 +12,12 @@ import AuthRoute from "./route/AuthRoute.js";
 dotenv.config();
 
 const app = express();
+
+const sessionStore =SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db:db
+})
 
 // (async () => {
 //     await db.sync();
@@ -21,6 +28,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
+    store:store,
     cookie:{
         secure:'auto',
     }
@@ -36,6 +44,8 @@ app.use(UserRoute);
 app.use(FarmingRoute);
 app.use(CropRoute);
 app.use(AuthRoute);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT,() => {
     console.log("Server up and running...");
