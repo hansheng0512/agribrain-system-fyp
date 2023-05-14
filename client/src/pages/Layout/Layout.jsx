6cba4e9407567f147bc80f9e9f0afacba0d361d4
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-14 16:31:24 +0800

    :lipstick: Change Layout and Add Username

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import Navbar from "../../component/layout/Navbar";
 import Sidebar from "../../component/layout/Sidebar";
+import {useSelector} from "react-redux";
+import {IoPerson} from 'react-icons/io5'
 
 const Layout = ({children}) => {
+    const {user} = useSelector((state) => state.auth);
+
     return (
         <React.Fragment>
             <Navbar/>
-            <div className="columns mt-6" style={{minHeight:"100vh"}}>
+            <div className="columns mt-6" style={{minHeight:"90vh"}}>
                 <div className="column is-2">
                     <Sidebar/>
                 </div>
                 <div className="column has-background-light">
-                    <main>{children}</main>
+                    <p style={{marginTop:"1vh",textAlign: "right",padding:"1vh",marginRight:"4vw"}}>
+                        <IoPerson style={{marginRight: "1vw"}}/>
+                        {user && user.user.user_fullname}
+                    </p>
+                    <main>
+                        <div style={{marginTop:"1vh",marginRight:"2vw",marginLeft:"2vw",marginBottom:"3vh",minHeight:"85vh",backgroundColor:"white",padding:"3vh"}}>
+                            {children}
+                        </div>
+                    </main>
                 </div>
             </div>
         </React.Fragment>
