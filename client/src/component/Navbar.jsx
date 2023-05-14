e0c478ed9ffd5ef135106dbaf34d1b4eb23fa8e1
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-14 13:04:45 +0800

    :lipstick: Let user login and logout

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
-import logo from '../logo.png'
+import {NavLink, useNavigate} from 'react-router-dom'
+import {useDispatch, useSelector} from "react-redux";
+import {LogoutUser, reset} from "../features/authSlice";
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav className="navbar is-fixed-top has-shadow is-success" >
-              <div className="navbar-brand">
-                <NavLink to="/dashboard" className="navbar-item" >
-                  <h1 style={{color:"white",fontSize:"30px",fontWeight:"900"}}>AgriBrain</h1>
-                </NavLink>
-            
-                <a href='!#' role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                  <span aria-hidden="true"></span>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {user} = useSelector((state) => state.auth);
+
+  const logout = () => {
+    dispatch(LogoutUser());
+    dispatch(reset());
+    navigate("/");
+  }
+  return (
+      <div>
+        <nav className="navbar is-fixed-top has-shadow is-success">
+          <div className="navbar-brand">
+            <NavLink to="/dashboard" className="navbar-item">
+              <h1 style={{color: "white", fontSize: "30px", fontWeight: "900"}}>AgriBrain</h1>
+            </NavLink>
+
+            <a href='!#' role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+               data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
                   <span aria-hidden="true"></span>
                   <span aria-hidden="true"></span>
                 </a>
@@ -22,7 +33,7 @@ const Navbar = () => {
                 <div className="navbar-end">
                   <div className="navbar-item">
                     <div className="buttons">
-                      <button className="button">
+                      <button onClick={logout} className="button">
                         Logout
                       </button>
                     </div>
--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
-import {IoPerson, IoHome,IoLogOut, IoPricetag} from 'react-icons/io5'
+import {NavLink, useNavigate} from "react-router-dom";
+import {IoHome, IoLogOut, IoPerson, IoPricetag} from 'react-icons/io5'
+import {useDispatch, useSelector} from "react-redux";
+import {LogoutUser, reset} from "../features/authSlice";
 
 const Sidebar = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const {user} = useSelector((state) => state.auth);
+
+    const logout = () => {
+        dispatch(LogoutUser());
+        dispatch(reset());
+        navigate("/");
+    }
     return (
         <div>
-            <aside className="menu pl-2 has-shadow" >
+            <aside className="menu pl-2 has-shadow">
                 <p className="menu-label mt-2">
                     General
                 </p>
@@ -13,22 +24,26 @@ const Sidebar = () => {
                     <li><NavLink to={"/dashboard"}> <IoHome/> Dashboard</NavLink></li>
                     <li><NavLink to={"/crop"}><IoPricetag/> Crops</NavLink></li>
                 </ul>
-                <p className="menu-label">
-                    Farmer
-                </p>
-                <ul className="menu-list">
-                    <li>
-                        <li><NavLink to={"/user"}><IoPerson/> User</NavLink></li>
-                    </li>
-                    <li>
+                {user && user.user.user_role === 'Farmer' && (
+                    <div>
+                        <p className="menu-label">
+                            Administration
+                        </p>
+                        <ul className="menu-list">
+                            <li>
+                                <li><NavLink to={"/user"}><IoPerson/> User</NavLink></li>
+                            </li>
+                            <li>
 
-                    </li>
-                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                )}
                 <p className="menu-label">
                     Settings
                 </p>
                 <ul className="menu-list">
-                    <button className="button is-white"><IoLogOut/> Logout</button>
+                    <button onClick={logout} className="button is-white"><IoLogOut/> Logout</button>
                 </ul>
             </aside>
         </div>
