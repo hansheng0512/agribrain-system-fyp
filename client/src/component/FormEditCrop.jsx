ba818dfa36aac5b39d5b90189fb761d212f5c856
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-14 13:51:46 +0800

    :lipstick: Edit Crop

--- client/src/component/FormEditCrop.jsx
+++ client/src/component/FormEditCrop.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from "axios";
+import {useNavigate, useParams} from "react-router-dom";
 
 const FormEditCrop = () => {
+    const [name,setName] = useState("");
+    const [message,setMessage] = useState("");
+    const {id} = useParams();
+
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const getCropById = async() => {
+            try{
+                const response = await axios.get(`http://localhost:5000/crop/${id}`)
+                setName(response.data.name);
+            }catch (error){
+                if(error.response){
+                    setMessage(error.response.data.message);
+                }
+            }
+        }
+        getCropById();
+    },[id])
+
+    const handleUpdateCrop = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.patch(`http://localhost:5000/crop/${id}`,{
+                crop_name:name,
+            })
+            navigate("/crop");
+        }catch (error) {
+            if(error.response){
+                setMessage(error.response.data.message);
+            }
+        }
+    }
+
     return (
         <div>
             <h1 className="title">Crop</h1>
@@ -8,16 +44,23 @@ const FormEditCrop = () => {
             <div className="card is-shadowless">
                 <div className="card-content">
                     <div className="content">
-                        <form>
+                        <form onSubmit={handleUpdateCrop}>
+                            <p className="has-text-centered">
+                                {message}
+                            </p>
                             <div className="field">
                                 <label className="label"> Crop Name</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='Crop Name'/>
+                                    <input
+                                        type="text"
+                                        className="input" value={name}
+                                        onChange={(e)=> setName(e.target.value)}
+                                        placeholder='Crop Name'/>
                                 </div>
                             </div>
                             <div className="field">
                                 <div className="control">
-                                    <button className="button is-success">
+                                    <button type="submit" className="button is-success">
                                         Update
                                     </button>
                                 </div>
