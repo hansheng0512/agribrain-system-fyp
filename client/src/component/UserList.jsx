36de34bcf563eb33741da0af1b0f042f875f2e4b
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-14 14:03:07 +0800

    :lipstick: Display User List

--- client/src/component/UserList.jsx
+++ client/src/component/UserList.jsx
@@ -1,29 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const UserList = () => {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = async () => {
+        const response = await axios.get("http://localhost:5000/user");
+        setUser(response.data);
+    };
+
+    const deleteUser = async (userId) => {
+        await axios.delete(`http://localhost:5000/user/${userId}`);
+        getUsers();
+    };
+
     return (
         <div>
             <h1 className="title">Users</h1>
-            <h2 className="subtitle">List of User</h2>
-            <table className='table is-striped is-fullwidth'>
-                <thread>
-                    <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Phone No</th>
-                        <th>Action</th>
-                    </tr>
-                </thread>
+            <h2 className="subtitle">List of Users</h2>
+            <Link to="/user/add" className="button is-primary mb-2">
+                Add New
+            </Link>
+            <table className="table is-striped is-fullwidth">
+                <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                {user.map((user, index) => (
+                    <tr key={user.user_uuid}>
+                        <td>{index + 1}</td>
+                        <td>{user.user_fullname}</td>
+                        <td>{user.user_email}</td>
+                        <td>{user.user_role}</td>
+                        <td>{user.user_phone}</td>
+                        <td>
+                            <Link
+                                to={`/user/edit/${user.uuid}`}
+                                className="button is-small is-info"
+                            >
+                                Edit
+                            </Link>
+                            <button
+                                onClick={() => deleteUser(user.uuid)}
+                                className="button is-small is-danger"
+                            >
+                                Delete
+                            </button>
+                        </td>
                     </tr>
+                ))}
                 </tbody>
             </table>
         </div>
