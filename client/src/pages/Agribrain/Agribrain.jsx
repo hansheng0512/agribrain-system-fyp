e90987df2a1fd9526fa959cb780473ef60a1dcd6
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-16 23:29:19 +0800

    :lipstick: Add AgriBrain Logo

--- client/src/pages/Agribrain/Agribrain.jsx
+++ client/src/pages/Agribrain/Agribrain.jsx
@@ -3,7 +3,7 @@ import Layout from '../Layout/Layout';
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {getMe} from "../../features/authSlice"
-import logo from "../../logo.png"
+import logo from "../../AgribrainLogo.png"
 import {Helmet} from "react-helmet";
 
 const Agribrain = () => {
@@ -26,7 +26,11 @@ const Agribrain = () => {
             <Helmet>
                 <title>AgriBrain | Welcome</title>
             </Helmet>
-            <img src={logo} alt="logo" />
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <img style={{ height: "auto", maxWidth: "60%" }} src={logo} alt="logo" />
+            </div>
+
+
             <p className="has-text-centered ">
                 AgriBrain is a cutting-edge Smart Farming and Agriculture system that utilizes IoT and Data Analytics to revolutionize the agriculture industry. Our system collects real-time data from sensors and devices installed on farms, which is then processed and analyzed using advanced machine learning algorithms. This allows farmers to make informed decisions based on insights gained from the data, such as optimizing crop yields, reducing water and fertilizer usage, and detecting plant diseases early. AgriBrain is a user-friendly and cost-effective solution that is designed to meet the needs of farmers, agribusinesses, and research institutions alike.
             </p>
