07387392962e8eb2559852f412ce4c78408c5286
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-16 19:33:55 +0800

    :lipstick: Change layout of breadcrumb

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Navbar from "../../component/layout/Navbar";
 import Sidebar from "../../component/layout/Sidebar";
-import {useSelector} from "react-redux";
-import {IoPerson} from 'react-icons/io5'
+import { useSelector } from "react-redux";
+import { IoPerson } from 'react-icons/io5';
 
-const Layout = ({children}) => {
-    const {user} = useSelector((state) => state.auth);
+const Layout = ({ children }) => {
+    const { user } = useSelector((state) => state.auth);
+    const location = useLocation();
+    const params = location.pathname.split('/').filter(param => param !== '');
+
+    const capitalize = (text) => {
+        const words = text.split('-');
+        const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+        return capitalizedWords.join(' ');
+    };
 
     return (
         <React.Fragment>
-            <Navbar/>
-            <div className="columns mt-6" style={{minHeight:"90vh"}}>
+            <Navbar />
+            <div className="columns mt-6" style={{ minHeight: "90vh" }}>
                 <div className="column is-2">
-                    <Sidebar/>
+                    <Sidebar />
                 </div>
                 <div className="column has-background-light">
                     <nav className="breadcrumb" aria-label="breadcrumbs">
-                        <p style={{marginTop:"1vh",textAlign: "right",padding:"1vh",marginRight:"4vw"}}>
-                            {user && user.user.user_fullname}
-                            <IoPerson style={{marginLeft: "1vw"}}/>
-                        </p>
-                        <ul>
-                            <li><a href="#">Bulma</a></li>
-                            <li><a href="#">Documentation</a></li>
-                            <li><a href="#">Components</a></li>
-                            <li className="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
-                        </ul>
+                        <div className="columns">
+                            <div className="column">
+                                <ul className="is-active" style={{ marginTop: "3vh",marginLeft:"3vh"}}>
+                                    <li>
+                                        <a style={{ pointerEvents: "none", color: "inherit" }}>Agribrain</a>
+                                    </li>
+                                    {params.map((param, index) => (
+                                        <li key={index}>
+                                            <a style={{ pointerEvents: "none", color: "inherit" }}>{capitalize(param)}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="column is-narrow">
+                                <p style={{ marginTop: "3vh", textAlign: "right",marginRight:"3vh" }}>
+                                    {user && user.user.user_fullname}
+                                    <IoPerson style={{ marginLeft: "1vw" }} />
+                                </p>
+                            </div>
+                        </div>
                     </nav>
 
                     <main>
-                        <div style={{marginTop:"1vh",marginRight:"2vw",marginLeft:"2vw",marginBottom:"3vh",minHeight:"85vh",backgroundColor:"white",padding:"3vh"}}>
+                        <div style={{ marginTop: "1vh", marginRight: "2vw", marginLeft: "2vw", marginBottom: "3vh", minHeight: "85vh", backgroundColor: "white", padding: "3vh" }}>
                             {children}
                         </div>
                     </main>
