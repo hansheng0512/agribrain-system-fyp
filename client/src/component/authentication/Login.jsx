dab120a45850181dcca12a88f51d8bdc6b6adcf7
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-16 21:38:55 +0800

    :lipstick: Add Modal

--- client/src/component/authentication/Login.jsx
+++ client/src/component/authentication/Login.jsx
@@ -1,14 +1,15 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {LoginUser, reset} from "../../features/authSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { LoginUser, reset } from "../../features/authSlice";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showEmptyFieldMessage, setShowEmptyFieldMessage] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const {user, isError, isSuccess, isLoading, message} = useSelector(
+    const { user, isError, isSuccess, isLoading, message } = useSelector(
         (state) => state.auth
     );
 
@@ -21,18 +22,63 @@ const Login = () => {
 
     const Auth = (e) => {
         e.preventDefault();
-        dispatch(LoginUser({email, password}));
+        if (email.trim() === "" || password.trim() === "") {
+            setShowEmptyFieldMessage(true);
+        } else {
+            setShowEmptyFieldMessage(false);
+            dispatch(LoginUser({ email, password }));
+        }
     };
 
     return (
         <section className="hero is-fullheight is-fullwidth">
+            <nav className="navbar is-fixed-top is-flex is-justify-content-center" style={{ backgroundColor: "#71AF9D" }}>
+                <div className="navbar-brand">
+                    <h1 className="navbar-item" style={{ color: "white",height:"15vh", fontSize: "50px", fontWeight: "900"}}>
+                        AgriBrain
+                    </h1>
+                </div>
+            </nav>
             <div className="hero-body">
                 <div className="container">
                     <div className="columns is-centered">
                         <div className="column is-4">
                             <form onSubmit={Auth} className="box">
-                                {isError && <p className="has-text-centered">{message}</p>}
-                                <h1 className="title is-2">Sign In</h1>
+                                {isError && (
+                                    <div className="modal is-active">
+                                        <div className="modal-background"></div>
+                                        <div className="modal-card" style={{width:"20vw"}}>
+                                            <header className="modal-card-head" style={{backgroundColor:"#71AF9D"}}>
+                                                <p className="modal-card-title" style={{color:"white", textAlign:"center",fontWeight:"bold"}}>Error</p>
+                                                <button
+                                                    className="delete"
+                                                    aria-label="close"
+                                                    onClick={() => dispatch(reset())}
+                                                ></button>
+                                            </header>
+                                            <section className="modal-card-body is-flex is-justify-content-center" style={{ fontSize:"20px"}}>
+                                                <p style={{margin:"2vh"}}>
+                                                    {message}
+                                                </p>
+                                                <br/>
+
+                                            </section>
+                                            <footer className="modal-card-foot is-justify-content-end" style={{height:"2vh"}}>
+                                                <button
+                                                    className="button is-small"
+                                                    onClick={() => dispatch(reset())}
+                                                    style={{backgroundColor:"#71AF9D",color:"white",alignContent:"right"}}
+                                                >
+                                                    OK
+                                                </button>
+                                            </footer>
+                                        </div>
+                                    </div>
+                                )}
+                                <h1 className="title is-2 has-text-centered">Sign In</h1>
+                                {showEmptyFieldMessage && (
+                                    <p className="help is-danger" style={{fontSize:"15px"}} >Please fill in all fields.</p>
+                                )}
                                 <div className="field">
                                     <label className="label">Email</label>
                                     <div className="control">
@@ -60,7 +106,8 @@ const Login = () => {
                                 <div className="field mt-5">
                                     <button
                                         type="submit"
-                                        className="button is-success is-fullwidth"
+                                        className="button is-fullwidth"
+                                        style={{backgroundColor:"#71AF9D",color:"white"}}
                                     >
                                         {isLoading ? "Loading..." : "Login"}
                                     </button>
