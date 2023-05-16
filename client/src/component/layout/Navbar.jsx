2b8bb4b006e2e8ab9b0a93e4498fd0dfa789fddd
Vincci-Zee Ching <chingc0606@gmail.com>
2023-05-16 22:22:09 +0800

    :lipstick: Change NavBar hover color

--- client/src/component/layout/Navbar.jsx
+++ client/src/component/layout/Navbar.jsx
@@ -1,57 +1,92 @@
-import React, {useState} from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [isBurgerOpen, setIsBurgerOpen] = useState(false);
 
   const toggleBurger = () => {
     setIsBurgerOpen(!isBurgerOpen);
-  }
-
+  };
 
   const menuItemStyle = {
-    color: "white",
-    hover:"none",
+    color: 'white',
+    backgroundColor: '#71AF9D'
+  };
+
+  const activeMenuItemStyle = {
+    color: 'white',
+    backgroundColor: '#71AF9D'
   };
 
   return (
       <div>
-        <nav className="navbar is-fixed-top " style={{backgroundColor: "#71AF9D"}}>
+        <nav className="navbar is-fixed-top has-shadow is-transparent" style={{ backgroundColor: '#71AF9D' }}>
           <div className="navbar-brand">
             <NavLink to="/agribrain" className="navbar-item">
-              <h1 style={{color: "white", fontSize: "35px", fontWeight: "900", marginLeft:"1vw"}}>AgriBrain</h1>
+              <h1 style={{ color: 'white', fontSize: '35px', fontWeight: '900', marginLeft: '1vw' }}>AgriBrain</h1>
             </NavLink>
-            <a role="button" className={`navbar-burger burger ${isBurgerOpen ? 'is-active' : ''}`} aria-label="menu"
-               aria-expanded="false"
-               data-target="navbarBasicExample"
-               onClick={toggleBurger}
-               style={{color:"white", marginTop:"2vh"}}
+            <a
+                role="button"
+                className={`navbar-burger burger ${isBurgerOpen ? 'is-active' : ''}`}
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={toggleBurger}
+                style={{ color: 'white', marginTop: '2vh' }}
             >
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
             </a>
           </div>
-
-          <div id="navbarBasicExample" className={`navbar-menu ${isBurgerOpen ? 'is-active' : ''}`} style={{backgroundColor:"#71AF9D"}}>
-            <div className="navbar-end" style={{fontWeight: 'bold', marginRight: "2vw"}}>
-              <NavLink to="/dashboard" className="navbar-item" onClick={toggleBurger} style={menuItemStyle}>
+          <nav
+              id="navbarBasicExample"
+              className={`navbar-menu ${isBurgerOpen ? 'is-active' : ''}`}
+              style={{ backgroundColor: '#71AF9D' }}
+          >
+            <div className="navbar-end" style={{ fontWeight: 'bold', marginRight: '2vw' }}>
+              <NavLink
+                  to="/dashboard"
+                  className={`navbar-item ${isBurgerOpen ? 'is-active' : ''}`}
+                  onClick={toggleBurger}
+                  style={isBurgerOpen ? activeMenuItemStyle : menuItemStyle}
+              >
                 Dashboard
               </NavLink>
-              <NavLink to="/sensor-monitoring" className="navbar-item" onClick={toggleBurger} style={menuItemStyle}>
+              <NavLink
+                  to="/sensor-monitoring"
+                  className={`navbar-item ${isBurgerOpen ? 'is-active' : ''}`}
+                  onClick={toggleBurger}
+                  style={isBurgerOpen ? activeMenuItemStyle : menuItemStyle}
+              >
                 Sensor Monitoring
               </NavLink>
-              <NavLink to="/climate-condition" className="navbar-item" onClick={toggleBurger} style={menuItemStyle}>
+              <NavLink
+                  to="/climate-condition"
+                  className={`navbar-item ${isBurgerOpen ? 'is-active' : ''}`}
+                  onClick={toggleBurger}
+                  style={isBurgerOpen ? activeMenuItemStyle : menuItemStyle}
+              >
                 Climate Condition
               </NavLink>
-              <NavLink to="/precision-farming" className="navbar-item" onClick={toggleBurger} style={menuItemStyle}>
+              <NavLink
+                  to="/precision-farming"
+                  className={`navbar-item ${isBurgerOpen ? 'is-active' : ''}`}
+                  onClick={toggleBurger}
+                  style={isBurgerOpen ? activeMenuItemStyle : menuItemStyle}
+              >
                 Precision Farming
               </NavLink>
-              <NavLink to="/data-analytics" className="navbar-item" onClick={toggleBurger} style={menuItemStyle}>
+              <NavLink
+                  to="/data-analytics"
+                  className={`navbar-item ${isBurgerOpen ? 'is-active' : ''}`}
+                  onClick={toggleBurger}
+                  style={isBurgerOpen ? activeMenuItemStyle : menuItemStyle}
+              >
                 Data Analytics
               </NavLink>
             </div>
-          </div>
+          </nav>
         </nav>
       </div>
   );
